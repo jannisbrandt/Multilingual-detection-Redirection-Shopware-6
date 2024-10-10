ea609beac931cf5327357a2e6cef419d58e1ab84
Jannis Brandt <jbrandt@laptopc0165.intern.neusta.de>
2024-10-10 10:09:15 +0200

    add the middlware logic that handles the locale-based redirection

---
+++ SmartLocalRedirect/src/Middleware/LocalRedirectMiddleware.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace YourNamespace\Middleware;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class LocaleRedirectMiddleware implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.request' => 'onKernelRequest',
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+        $preferredLocale = $request->getPreferredLanguage(['de', 'en']);
+        $currentLocale = $request->getLocale();
+
+        if ($preferredLocale !== $currentLocale) {
+            if ($preferredLocale === 'en') {
+                $event->setResponse(new RedirectResponse('/en/'));
+            }
+        }
+    }
+}
