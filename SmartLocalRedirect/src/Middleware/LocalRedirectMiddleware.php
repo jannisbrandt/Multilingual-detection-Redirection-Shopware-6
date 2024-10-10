842b1eb14f0292d09b8dcab11a1785ce005ff38e
Jannis Brandt <jbrandt@laptopc0165.intern.neusta.de>
2024-10-10 10:16:19 +0200

    add the middlware logic that handles the locale-based redirection

--- SmartLocalRedirect/src/Middleware/LocalRedirectMiddleware.php
+++ SmartLocalRedirect/src/Middleware/LocalRedirectMiddleware.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace YourNamespace\Middleware;
+namespace SmartLocaleRedirect\Middleware;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
